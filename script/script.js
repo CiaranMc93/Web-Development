797dcc1b6a70315f592c857b1ebeb31d21158269
Ciaran McManus <ciaranmcmanus@live.ie>
2016-03-21 21:26:14 +0000

    Add cover image, add scrolling to correct 'pages'.

--- script/script.js
+++ script/script.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
-	/* This code is executed after the DOM has been completely loaded */
 
-	$('nav a,footer a.up').click(function(e){
 
-		// If a link has been clicked, scroll the page to the link's hash target:
+function scrollToArticle(id)
+{
+	console.log("Got here" + id);
 
-		$.scrollTo( this.hash || 0, 1500);
-		e.preventDefault();
-	});
+    $.scrollTo( "article[name='"+ id +"']" || 0, 1500);
+    preventDefault();
 
-});
+}
