bffae0dca297522532b672af0365a85eeb4dbf5a
Ciaran McManus <ciaranmcmanus@live.ie>
2016-03-23 17:53:05 +0000

    Up and down button bunctionality and placement.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+//create temporary variable to hold the current page
+//first page is the home page
+var temp = 'home';
+var i = 0;
+
+//prevent scroll to flashing
+$('.slideNav').click(function(e){
+        e.preventDefault();
+
+        console.log("got here");
+        
+        scrollToButtons();
+});
 
 //scrolls to the article
 function scrollToArticle(id)
@@ -11,7 +24,51 @@ function scrollToArticle(id)
 		$.scrollTo( "article[name='"+ id +"']" || 0, 1500);
 	}
     
-    //prevent the page from reloading and scrolling to the top
-    preventDefault();
-
 }
+
+//scroll to the previous or next slide based on current one
+function scrollToButtons(id)
+{
+	//page array
+	var pages = ['home','bio','experience','tech','hobbies'];
+
+	//check if the up or down button has been clicked
+	if(id == 'up')
+	{
+		if(temp != 'home')
+		{
+			//decrement the variable
+			i--;
+
+			temp = pages[i];
+
+			//scroll to the correct page.
+			$.scrollTo( "article[name='"+ temp +"']" || 0, 1500);
+		}
+	}
+	else
+	{
+		if(temp != 'hobbies')
+		{
+
+			//decrement the variable
+			i++;
+
+			temp = pages[i];
+
+			console.log("Down" + pages[i]);
+
+			//scroll to the correct page.
+			$.scrollTo( "article[name='"+ temp +"']" || 0, 1500);
+
+		}
+	}
+}
+
+//disable key scrolling
+window.addEventListener("keydown", function(e) {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
